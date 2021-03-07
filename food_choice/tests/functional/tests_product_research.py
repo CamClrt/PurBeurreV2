f4ca9a732238e579ed6f682e537589d875aedc57
CamClrt <camille.clarret@gmail.com>
2021-03-07 16:09:29 +0100

    refactor(functional tests): product_research + authentication

--- food_choice/tests/functional/tests_product_research.py
+++ food_choice/tests/functional/tests_product_research.py
@@ -1,32 +1,21 @@
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
-from selenium import webdriver
-
-from pur_beurre.settings import BASE_DIR
-
-firefox_options = webdriver.FirefoxOptions()
-firefox_options.headless = True
+from selenium.webdriver.firefox.webdriver import WebDriver
 
 
 class ProductResearchTest(StaticLiveServerTestCase):
-    """Functional tests using the Firefox web browser in headless mode."""
-
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
-        cls.driver = webdriver.Firefox(
-            executable_path=f"{BASE_DIR}/webdrivers/geckodriver",
-            options=firefox_options,
-        )
-        cls.driver.implicitly_wait(10)
-        cls.driver.maximize_window()
+        cls.selenium = WebDriver()
+        cls.selenium.implicitly_wait(10)
 
     @classmethod
     def tearDownClass(cls):
+        cls.selenium.quit()
         super().tearDownClass()
-        cls.driver.quit()
 
     def test_research_product_with_home_buttom(self):
-        self.driver.get("%s%s" % (self.live_server_url, "/"))
-        research_input = self.driver.find_element_by_id("home_search")
+        self.selenium.get("%s%s" % (self.live_server_url, "/"))
+        research_input = self.selenium.find_element_by_id("home_search")
         research_input.send_keys("Nutella")
-        self.driver.find_element_by_class_name("btn").click()
+        self.selenium.find_element_by_class_name("btn").click()
