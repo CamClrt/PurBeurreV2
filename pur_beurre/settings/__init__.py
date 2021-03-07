71311437c2b737e0ba341a867bfff3055feb0639
CamClrt <camille.clarret@gmail.com>
2021-03-07 15:39:22 +0100

    fix(settings): fix BASE_DIR path

--- pur_beurre/settings/__init__.py
+++ pur_beurre/settings/__init__.py
@@ -19,7 +19,9 @@ load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 # BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))  # noqa: E501
+BASE_DIR = os.path.dirname(
+    os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+)  # noqa: E501
 
 
 # Quick-start development settings - unsuitable for production
