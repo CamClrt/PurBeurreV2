5fde1a047822941c41d578fc93e982364edb2015
CamClrt <camille.clarret@gmail.com>
2021-03-23 07:44:42 +0100

    fix(profile model): replace to foreign key

--- users/models.py
+++ users/models.py
@@ -95,10 +95,15 @@ class Profile(models.Model):
     user = models.OneToOneField(
         User, on_delete=models.CASCADE, related_name="user_profile"
     )
-    diet = models.OneToOneField(
-        Diet, on_delete=models.CASCADE, related_name="diet_profile"
+    diet = models.ForeignKey(
+        Diet,
+        on_delete=models.CASCADE,
+        related_name="diet_profile",
     )
-    image = models.ImageField(default="default.jpg", upload_to="profile_pics")
+    image = models.ImageField(
+        default="default.jpg",
+        upload_to="profile_pics",
+    )
 
     def __str__(self):
         return f"{self.user.username} Profile"
